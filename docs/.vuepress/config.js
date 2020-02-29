7f381bbe82872fdb61bf695dcab247e943c7252b
尹金标 <731509863@qq.com>
2020-02-29 19:41:43 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,95 @@
+module.exports = {
+    title: "小二温不胜",
+    description: '温不胜的blog',
+    head: [//favicon配置
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
+    locales: {//多语言配置
+        '/': {
+            lang: 'zh-CN',
+        }
+    },
+    base:'/myblog/',
+    themeConfig: {//主题配置
+        repo: 'https://gitee.com/shuaibiao/myblog',
+        repoLabel: 'gitee',
+        editLinks: true,
+        lastUpdated: '上次更新', // string | boolean
+        displayAllHeaders: true,//展开所有侧边栏，默认为false
+        nav: [
+            {
+                text: '在线文档',
+                ariaLabel: 'Language Menu',
+                items: [
+                    { text: 'VuePress 中文文档', link: 'https://www.vuepress.cn/guide/' },
+                    { text: 'ElementUI', link: 'https://element.eleme.cn/#/zh-CN/component/installation' },
+                    { text: 'BootstrapVue', link: 'https://bootstrap-vue.js.org/' },
+                    { text: 'JQuery', link: 'http://jquery.cuishifeng.cn/' }
+                ]
+            },
+            {
+                text: '在线工具',
+                ariaLabel: 'Language Menu',
+                items: [
+                    { 
+                        text: '在线编辑', 
+                        items: [
+                            { text: 'PDF 转换器', link: 'https://smallpdf.com/cn/pdf-to-word' },
+                            { text: 'JSON 编辑器', link: 'https://www.bejson.com/jsoneditoronline' },
+                            { text: 'MD 表格生成', link: 'https://tableconvert.com/?output=text' },
+                            { text: 'CRON 表达式', link: 'http://cron.qqe2.com/' },    
+                            { text: '代码格式化', link: 'https://tool.oschina.net/codeformat/html' },
+                            { text: '公式编辑器', link: 'https://zh.numberempire.com/latexequationeditor.php' },
+                            { text: '二维码生成器', link: 'https://cli.im/' },
+                            { text: '在线编码转换', link: 'http://tool.chinaz.com/tools/native_ascii.aspx' },    
+                            { text: 'PDF转换器', link: 'https://smallpdf.com/cn/pdf-to-word' }
+                        ] 
+                    },
+                    {
+                        text: '在线服务', 
+                        items: [
+                            { text: 'BOOT CDN', link: 'https://www.bootcdn.cn/' },
+                            { text: '微信 CDN', link: 'https://qydev.weixin.qq.com/cdn/cdnjs.html' },
+                            { text: '科大谷歌字体加速', link: 'https://servers.ustclug.org/2014/06/blog-googlefonts-speedup/' },    
+                            { text: 'tldr 命令手册', link: 'https://tldr.ostera.io/' },    
+                        ] 
+                    },
+                    {
+                        text: '开源镜像', 
+                        items: [
+                            { text: 'OPSX', link: 'https://developer.aliyun.com/mirror/' },
+                            { text: 'AZURE', link: 'https://mirror.azure.cn/' },
+                            { text: 'Docker HUB', link: 'https://tableconvert.com/?output=text' }
+                        ] 
+                    },
+                    {
+                        text: '趋势分析', 
+                        items: [
+                            { text: '谷歌趋势', link: 'https://trends.google.com/trends/?geo=US' },
+                            { text: '百度指数', link: 'http://index.baidu.com/v2/index.html#/' },
+                            { text: '百度流量研究院', link: 'https://tongji.baidu.com/research/' }
+                        ] 
+                    }
+                  
+                ]
+            },
+          { text: '联系我', link: 'http://wpa.qq.com/msgrd?v=3&uin=731509863&site=qq&menu=yes' }
+        ],
+        sidebar: {
+            //前端部分笔记
+            '/front/': [
+              '',     /* /front/ */
+
+            ],      
+            //后端部分笔记
+            '/backend/': [
+              '',      /* /backend/ */
+            ],      
+            // fallback
+            '/': [
+              '',        /* / */
+              'about'    /* /about.html */
+            ]
+        }
+      }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /avatar.png
+heroText: 温不胜
+tagline: 温故而知新，可以为师矣。
+actionText: 开始回顾
+actionLink: /guide/
+features:
+- title: 标题1
+  details: 描述1.
+- title: 标题2
+  details: 描述2.
+- title: 标题3
+  details: 描述3.
+footer: MIT Licensed | Copyright © 2020-present
+---

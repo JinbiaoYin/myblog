601dbc4bb6a3cf52742044a8285b049a8dfd1ea6
1 <731509863@qq.com>
2020-03-11 11:19:00 +0800

    config配置优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,10 @@
+const head = require('../../config/headconfig.js')
 const plugins = require('../../config/pluginconfig.js')
 const nav = require('../../config/navconfig.js')
 module.exports = {
     title: "小二温不胜",
     description: '温不胜的blog',
-    head: [//favicon配置
-        ['link', { rel: 'icon', href: '/favicon.ico' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }]   
-    ],
+    head: head,
     plugins: plugins,
     themeConfig: {//主题配置
         repo: 'JinbiaoYin/myblog',
